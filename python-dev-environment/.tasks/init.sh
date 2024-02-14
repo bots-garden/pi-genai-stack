c63c9b163d1f177fbacbc8a642c82c793d99d563
@k33g <ph.charriere@gmail.com>
2024-02-14 04:29:16 +0100

    ✨ Introduce new feature(s): python ide

---
+++ python-dev-environment/.tasks/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ------------------------------------
+# Install Code Server Extensions
+# ------------------------------------
+code-server --install-extension wesbos.theme-cobalt2
+code-server --install-extension PKief.material-icon-theme
+code-server --install-extension PKief.material-product-icons
+code-server --install-extension aaron-bond.better-comments
+code-server --install-extension gitpod.gitpod-theme
+code-server --install-extension ms-python.python
+
+pip install --upgrade -r workspace/requirements.txt
+
+echo "🌍 open: http://0.0.0.0:${PYTHON_APP_TPL_HTTP_PORT}"
---
+++ python-dev-environment/workspace/demo2.py
@@ -0,0 +1,20 @@
+import os
+
+from langchain_community.llms import ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+ollama_base_url = os.getenv("OLLAMA_BASE_URL")
+
+llm = ollama.Ollama(
+    base_url=ollama_base_url, 
+    model='tinydolphin', 
+    callback_manager= CallbackManager([StreamingStdOutCallbackHandler()])
+)
+
+prompt = "Who is Bram Stoker?"
+
+llm.invoke(prompt)
+
+print('\n')
+
